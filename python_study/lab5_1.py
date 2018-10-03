be517d59a16f9848c5c213d67de1d8245611edc1
MojitoBar <wnehdtjr5@naver.com>
2018-10-03 21:40:32 +0900

    update 1003

---
+++ python_study/lab5_1.py
@@ -0,0 +1,15 @@
+"""
+챕터: day5
+주제: 함수
+문제:
+두 개의 정수를 매개변수로  받아서, 두 수의 차를 반환하는 함수 substract를 정의한다.
+4, 8을 매개변수로 substract를 호출한 후 결과를 출력한다.
+작성자: 주동석
+작성일: 2018. 10. 2
+"""
+# 두 수의 차를 반환하는 substract 함수 정의
+def substract(a, b):
+    return a - b
+
+# 함수 호출
+print(substract(4, 8))
